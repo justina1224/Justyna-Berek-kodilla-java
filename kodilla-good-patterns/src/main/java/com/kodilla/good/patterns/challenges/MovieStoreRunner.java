684b71472a8ee6759568decbb732588b166a9b13
justina1224 <justyna.pilzno@gmail.com>
2018-07-09 22:59:42 +0200

    9.1. Movies Map Stream

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreRunner.java
@@ -0,0 +1,16 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MovieStoreRunner {
+    public static void main (String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        String titles = movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining("!","",""));
+
+        System.out.println(titles);
+
+    }
+}
