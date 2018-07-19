13e1114d6ebb46a9999c8986d8f1c76225dc3d2c
justina1224 <justyna.pilzno@gmail.com>
2018-07-19 22:01:37 +0200

    10.2 Calculator display

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,61 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+    @Test
+    public void testAddCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.add(6,3);
+        //Then
+        Assert.assertEquals(9, result, 0);
+    }
+
+    @Test
+    public void testSubCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.sub(6,3);
+        //Then
+        Assert.assertEquals(3, result, 0);
+    }
+
+    @Test
+    public void testMulCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.mul(6,3);
+        //Then
+        Assert.assertEquals(18, result, 0);
+    }
+
+    @Test
+    public void testDivCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.div(6,3);
+        //Then
+        Assert.assertEquals(2, result, 0);
+    }
+}
