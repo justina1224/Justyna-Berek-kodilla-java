e28d3e14ad11ccd96633f2738837d7d0ed9a2bec
justina1224 <justyna.pilzno@gmail.com>
2018-08-29 08:58:49 +0200

    13.4 Query

--- kodilla-hibernate1/src/main/java/com/kodilla/hibernate1/manytomany/Company.java
+++ kodilla-hibernate1/src/main/java/com/kodilla/hibernate1/manytomany/Company.java
@@ -5,6 +5,13 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(
+        name = "Company.retrieveCompaniesWithNameBeginningWith",
+        query = "SELECT * FROM COMPANIES" +
+                " WHERE LEFT(COMPANY_NAME, 3) = :FIRST_THREE_LETTERS",
+        resultClass = Company.class
+)
+
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
--- kodilla-hibernate1/src/test/java/com/kodilla/hibernate1/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate1/src/test/java/com/kodilla/hibernate1/manytomany/dao/CompanyDaoTestSuite.java
@@ -9,11 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CompanyDaoTestSuite {
     @Autowired
     CompanyDao companyDao;
+    @Autowired
+    EmployeeDao employeeDao;
 
     @Test
     public void testSaveManyToMany(){
@@ -45,11 +49,15 @@ public class CompanyDaoTestSuite {
         int dataMaestersId = dataMaesters.getId();
         companyDao.save(greyMatter);
         int greyMatterId = greyMatter.getId();
+        List<Employee> employeesWithName = employeeDao.retrieveEmployeeWithName("John");
+        List<Company> companiesWithNameBeginningWith = companyDao.retrieveCompaniesWithNameBeginningWith("Dat");
 
         //Then
         Assert.assertNotEquals(0, softwareMachineId);
         Assert.assertNotEquals(0, dataMaestersId);
         Assert.assertNotEquals(0, greyMatterId);
+        Assert.assertEquals(1, employeesWithName.size());
+        Assert.assertEquals(1, companiesWithNameBeginningWith.size());
 
         //CleanUp
         try {
